7d868ff352d9e08aa20e9fd7584d6df6260d6602
francosampieri <francosampiee@gmail.com>
2025-09-09 17:27:48 -0300

    añado menu a todos los tp y termino tp5

--- Unidad 1/Franco Sampieri - Actividad 1 Estructuras Secuenciales 1PRO1.py
+++ Unidad 1/Franco Sampieri - Actividad 1 Estructuras Secuenciales 1PRO1.py
@@ -1,69 +1,95 @@
 import math
+def validar_int_rango(mensaje,rango):
+    num = input(mensaje)
+    while not num.isnumeric() or int(num) not in range(rango[0],rango[1]+1): #valida primero si es un numero y despues si esta en el rango
+        print("ERROR: Elige un numero entero dentro del rango")
+        num = input(mensaje)
+    return int(num)
 
-print("Hola Mundo!")
+def menu_ejercicios(funciones):
+    """Menu para seleciconar ejercicios\n
+    Recibe list con las funciones de cada ejercicio empezando por None
+    """
+    seguir = True
+    while seguir:
+        ejercicio_elegido = validar_int_rango(f"Ingrese el numero de ejercicio que desea ejecutar (1-{len(funciones)-1}) [0] para terminar ", [0, len(funciones)-1])
+        if ejercicio_elegido == 0: 
+            seguir = False
+            continue
+        else: 
+            print(f"\n -----EJERCICIO {ejercicio_elegido}-----")
+            funciones[ejercicio_elegido]()
+            print() #salto de linea
+            input("Presione ENTER para volver") #para no sobrecargar consola
 
+def ejercicio_1():
+    print("Hola Mundo!")
 
-nombre = input("Ingrese su nombre ")
-print (f"Hola {nombre}!")
+def ejercicio_2():
+    nombre = input("Ingrese su nombre ")
+    print (f"Hola {nombre}!")
 
+def ejercicio_3():
+    nombre = input("Ingrese su nombre ")
+    apellido = input("Ingrese su apellido ")
+    edad = int(input("Ingrese su edad "))
+    residencia = input("Ingrese su lugar de residencia ")
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
-nombre = input("Ingrese su nombre ")
-apellido = input("Ingrese su apellido ")
-edad = int(input("Ingrese su edad "))
-residencia = input("Ingrese su lugar de residencia ")
-print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+def ejercicio_4():
+    radio = int(input("Ingrese el radio del circulo "))
+    perimetro = 2 * math.pi  * radio
+    area = math.pi * (radio**2)
+    print(f"El perimetro del circulo es {perimetro} y el area es {area}")
 
+def ejercicio_5():
+    segundos = int(input("Ingrese los segundos para calcular en horas "))
+    horas = segundos / 60 / 60
+    print(f"{segundos} segundos equivalen a {horas} horas")
 
-radio = int(input("Ingrese el radio del circulo "))
-perimetro = 2 * math.pi  * radio
-area = math.pi * (radio**2)
-print(f"El perimetro del circulo es {perimetro} y el area es {area}")
+def ejercicio_6():
+    numero = int(input("Ingrese un numero para conocer su tabla de multiplicar "))
+    print(f"{numero} X 1 = {numero}")
+    print(f"{numero} X 2 = {numero * 2}")
+    print(f"{numero} X 3 = {numero * 3}")
+    print(f"{numero} X 4 = {numero * 4}")
+    print(f"{numero} X 5 = {numero * 5}")
+    print(f"{numero} X 6 = {numero * 6}")
+    print(f"{numero} X 7 = {numero * 7}")
+    print(f"{numero} X 8 = {numero * 8}")
+    print(f"{numero} X 9 = {numero * 9}")
+    print(f"{numero} X 10 = {numero * 10}")
 
+def ejercicio_7():
+    numero1 = int(input("Ingresa el primer numero entero distinto de cero "))
+    numero2 = int(input("Ingresa el segundo numero entero distinto de cero "))
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    division = numero1 / numero2
+    multiplicacion = numero1 * numero2
+    print(f"{numero1} + {numero2} = {suma}")
+    print(f"{numero1} - {numero2} = {resta}")
+    print(f"{numero1} / {numero2} = {division}")
+    print(f"{numero1} * {numero2} = {multiplicacion}")
 
-segundos = int(input("Ingrese los segundos para calcular en horas "))
-horas = segundos / 60 / 60
-print(f"{segundos} segundos equivalen a {horas} horas")
+def ejercicio_8():
+    peso_usuario = float(input("Ingresa tu peso en kilogramos "))
+    altura_usuario =  float(input("Ingresa tu altura en centimetros ")) / 100 #centimetros a metros
+    indice_masa_corporal = peso_usuario / (altura_usuario**2)
+    print(f"Tu indice de masa corporal es {indice_masa_corporal}")
 
+def ejercicio_9():
+    temp_celsius = float(input("Ingrese la temperatura en grados celsius "))
+    temp_fahrenheit = temp_celsius * (9/5) + 32
+    print(f"{temp_celsius}º Celcius equivalen a {temp_fahrenheit}º Fahrenheit")
 
-numero = int(input("Ingrese un numero para conocer su tabla de multiplicar "))
-print(f"{numero} X 1 = {numero}")
-print(f"{numero} X 2 = {numero * 2}")
-print(f"{numero} X 3 = {numero * 3}")
-print(f"{numero} X 4 = {numero * 4}")
-print(f"{numero} X 5 = {numero * 5}")
-print(f"{numero} X 6 = {numero * 6}")
-print(f"{numero} X 7 = {numero * 7}")
-print(f"{numero} X 8 = {numero * 8}")
-print(f"{numero} X 9 = {numero * 9}")
-print(f"{numero} X 10 = {numero * 10}")
+def ejercicio_10():
+    numero1 = float(input("Ingrese el primer numero "))
+    numero2 = float(input("Ingrese el segundo numero "))
+    numero3 = float(input("Ingrese el tercer numero "))
+    cant_numeros = 3
+    promedio = (numero1 + numero2 + numero3) / cant_numeros
+    print(f"El promedio entre {numero1}, {numero2} y {numero3} es {promedio}")
 
-
-numero1 = int(input("Ingresa el primer numero entero distinto de cero "))
-numero2 = int(input("Ingresa el segundo numero entero distinto de cero "))
-suma = numero1 + numero2
-resta = numero1 - numero2
-division = numero1 / numero2
-multiplicacion = numero1 * numero2
-print(f"{numero1} + {numero2} = {suma}")
-print(f"{numero1} - {numero2} = {resta}")
-print(f"{numero1} / {numero2} = {division}")
-print(f"{numero1} * {numero2} = {multiplicacion}")
-
-
-peso_usuario = float(input("Ingresa tu peso en kilogramos "))
-altura_usuario =  float(input("Ingresa tu altura en centimetros ")) / 100 #centimetros a metros
-indice_masa_corporal = peso_usuario / (altura_usuario**2)
-print(f"Tu indice de masa corporal es {indice_masa_corporal}")
-
-
-temp_celsius = float(input("Ingrese la temperatura en grados celsius "))
-temp_fahrenheit = temp_celsius * (9/5) + 32
-print(f"{temp_celsius}º Celcius equivalen a {temp_fahrenheit}º Fahrenheit")
-
-
-numero1 = float(input("Ingrese el primer numero "))
-numero2 = float(input("Ingrese el segundo numero "))
-numero3 = float(input("Ingrese el tercer numero "))
-cant_numeros = 3
-promedio = (numero1 + numero2 + numero3) / cant_numeros
-print(f"El promedio entre {numero1}, {numero2} y {numero3} es {promedio}")
+funciones = [None, ejercicio_1, ejercicio_2, ejercicio_3, ejercicio_4, ejercicio_5, ejercicio_6, ejercicio_7, ejercicio_8, ejercicio_9, ejercicio_10]
+menu_ejercicios(funciones)
--- Unidad 5/Franco Sampieri - Actividades 5 Listas 1PRO1.py
+++ Unidad 5/Franco Sampieri - Actividades 5 Listas 1PRO1.py
@@ -1,59 +1,77 @@
-"""EJERCICIO 1"""
-multiplos_cuatro = list(range(4,101,4))
-print(multiplos_cuatro)
+def validar_int_rango(mensaje,rango):
+    num = input(mensaje)
+    while not num.isnumeric() or int(num) not in range(rango[0],rango[1]+1): #valida primero si es un numero y despues si esta en el rango
+        print("ERROR: Elige un numero entero dentro del rango")
+        num = input(mensaje)
+    return int(num)
 
-"""EJERCICIO 2"""
-# Crear una lista con cinco elementos (colocar los que más te gusten) y mostrar el penúltimo.
-mi_lista = [1, 2, 3, 4, 5]
-print(mi_lista[-2])
+def menu_ejercicios(funciones):
+    """Menu para seleciconar ejercicios\n
+    Recibe list con las funciones de cada ejercicio empezando por None
+    """
+    seguir = True
+    while seguir:
+        ejercicio_elegido = validar_int_rango(f"Ingrese el numero de ejercicio que desea ejecutar (1-{len(funciones)-1}) [0] para terminar ", [0, len(funciones)-1])
+        if ejercicio_elegido == 0: 
+            seguir = False
+            continue
+        else: 
+            print(f"\n -----EJERCICIO {ejercicio_elegido}-----")
+            funciones[ejercicio_elegido]()
+            print() #salto de linea
+            input("Presione ENTER para volver") #para no sobrecargar consola
 
-"""EJERCICIO 3"""
-# Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por pantalla.
-lista = []
-lista.append("palabra")
-lista.append("hola")
-lista.append("chau")
-print(lista)
+def ejercicio_1():
+    multiplos_cuatro = list(range(4,101,4))
+    print(multiplos_cuatro)
 
-"""EJERCICIO 4"""
-animales = ["perro", "gato", "conejo", "pez"]
-animales[-1] = "oso"
-animales[-2] = "loro"
-print(animales)
+def ejercicio_2():
+    mi_lista = [1, 2, 3, 4, 5]
+    print(mi_lista[-2])
 
-"""EJERCICIO 5"""
-print("Ese programa remueve de la lista el numero mas grande, en este caso el 22")
+def ejercicio_3():
+    lista = []
+    lista.append("palabra")
+    lista.append("hola")
+    lista.append("chau")
+    print(lista)
 
-"""EJERCICIO 6"""
-lista = list(range(10,31,5))
-print(lista[:1])
+def ejercicio_4():
+    animales = ["perro", "gato", "conejo", "pez"]
+    animales[-1] = "oso"
+    animales[-2] = "loro"
+    print(animales)
 
-"""EJERCICIO 7"""
-autos = ["sedan", "polo", "suran", "gol"]
-autos[1:3] = ["fiesta", "sandero"]
-print(autos)
+def ejercicio_5():
+    print("Ese programa remueve de la lista el numero mas grande, en este caso el 22")
 
-"""EJERCICIO 8"""
-dobles = []
-for i in range(5,16,5):
-    dobles.append(i*2)
-print(dobles)
+def ejercicio_6():
+    lista = list(range(10,31,5))
+    print(lista[:2])
 
-"""EJERCICIO 9"""
-compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
-compras[2].append("jugo")
-compras[1][1] = "tallarines"
-compras[0].remove("pan")
-print(compras)
+def ejercicio_7():
+    autos = ["sedan", "polo", "suran", "gol"]
+    autos[1:3] = ["fiesta", "sandero"]
+    print(autos)
 
-"""EJERCICIO 10"""
-# Elaborar una lista anidada llamada “lista_anidada” con los siguientes elementos:
-# [0] = 15
-# [1] = True
-# [2][0] = 25.5
-# [2][1] = 57.9
-# [2][2] = 30.6
-# [3] = False
-# Imprimir la lista resultante.
-lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
-print (lista_anidada)
+def ejercicio_8():
+    dobles = []
+    for i in range(5,16,5):
+        dobles.append(i*2)
+    print(dobles)
+
+def ejercicio_9():
+    compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+    compras[2].append("jugo")
+    compras[1][1] = "tallarines"
+    compras[0].remove("pan")
+    print(compras)
+
+def ejercicio_10():
+    lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+    print (lista_anidada)
+
+
+#main
+funciones = [None, ejercicio_1, ejercicio_2, ejercicio_3, ejercicio_4, ejercicio_5, ejercicio_6, ejercicio_7, ejercicio_8, ejercicio_9, ejercicio_10]
+menu_ejercicios(funciones)
